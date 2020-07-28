138e284572c012034911d691a190820103212988
nicsFantasy <vinicius.edwards@gmail.com>
2020-07-28 17:13:20 -0400

    update na resposta

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/home', (req, res) => {
-    return res.json({ message: 'hello world' });
+    return res.send('hello world');
 });
 
 export default routes;
